afd344727c84025fbe6828f713ed67df8ecd6f82
khushal <khushalsuthar546@gmail.com>
2024-09-27 09:51:48 +0530

    done vercel setting and vercel.json and built_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 echo "BUILD START"
-python3.9 -m pip  install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-echo "BULID END"
+echo "BUILD END"
+
+mkdir -p staticfiles_build
+cp -r static/* staticfiles_build/
